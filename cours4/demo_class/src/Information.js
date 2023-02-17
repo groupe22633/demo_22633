d30494220d51d9299c8055252e411348d687b4f4
Jonathan Martel <jonathmartel@gmail.com>
2023-02-17 11:59:25 -0500

    Commentaire cours 4

--- cours4/demo_class/src/Information.js
+++ cours4/demo_class/src/Information.js
@@ -33,41 +33,54 @@ export default class Information{
         }
     ]
 
-
+    /**
+     * Constructeur
+     * @param {DOMElement} elementParent  Point d'insertion du DOM
+     * @param {number} nbCoursMax Valeur max pour "Nombre de cours"
+     */
     constructor(elementParent, nbCoursMax){
         this.nbCoursMax = nbCoursMax;
         this.elementParent = elementParent;
         //this.validation();
     }
-
+    /**
+     * permet de lancer la validation du composant. Appelé par App
+     * @returns boolean
+     */
     validation(){
         let aValide =[];
         let valide = true;
+        
+        // pour chaque élément dans notre formulaire
         for(let elementForm of this.formInfo){
-            elementForm.valeur = document.querySelector(`[name='${elementForm.nom}']`).value;
+            elementForm.valeur = document.querySelector(`[name='${elementForm.nom}']`).value;   // Lire la valeur
             
+            // Est-ce qu'il y a un tableau ou une fonction de validation
             if(Array.isArray(elementForm.fctValidation)){
+                // Appel chaque fonction une après l'autre
                 for(let fct of elementForm.fctValidation){
-                    aValide.push(fct.bind(elementForm)());    
+                    aValide.push(fct.bind(elementForm)());    // Appel de chaque fonction, en lui donnant explicitement la référence à this (l'objet dans le tableau formInfo)
                 }
-            }else{
+            }else if(elementForm.fctValidation){ // Une seule fonction de validation
                 aValide.push(elementForm.fctValidation());
             }
+            
         }
-        console.log("validation")
+        //console.log("validation")
+        // Si tous les champs sont valides
         valide = aValide.every(function(element){
             return (element === true);
-        })
-        //console.log(indexFalse)
-        /*if() {
-                        
-            valide = true;
-        }*/
-        console.log(valide)
+        });
+       
+        //console.log(valide)
         return valide;
     }
-
+    /**
+     * Retourne les informations que le composant expose
+     * @returns {objet} 
+     */
     getData(){
+        // Récupère la valeur de "nbCours"
         let data = this.formInfo.find(function(element){
             let bTrouve = false;
             if(element.nom == "nbCours"){
@@ -79,6 +92,9 @@ export default class Information{
         return {nbCours : data.valeur};
     }
 
+     /**
+     * Méthode d'affichage du composant
+     */
     afficher(){
         let chaineHTML = `<fieldset class="information">
                             <legend>Informations personnelles</legend>`;
@@ -97,25 +113,31 @@ export default class Information{
         
 
     }
-
+    /**
+     * Méthode de validation des valeurs
+     * @returns boolean
+     */
     nonVide (){
-        console.log(this.valeur);
+        //console.log(this.valeur);
         let valide = false;
         if(this.valeur != ""){
             valide = true;
         }
-        console.log(valide)
+        //console.log(valide)
         return valide;
     }
-
+    /**
+     * Méthode de validation des valeurs
+     * @returns boolean
+     */
     texte(){
-        console.log(this)
+        //console.log(this)
         let valide = false;
         
         if(this.valeur.length >=2){
             valide = true;
         }
-        console.log(valide)
+        //console.log(valide)
         return valide;
     }
 }
--- cours4/demo_class/src/Inscription.js
+++ cours4/demo_class/src/Inscription.js
@@ -1,35 +1,63 @@
 import { cours } from "./data.js";
 export default class Inscription{
+    /**
+     * nbCours max
+     * Valeur initiale de la propriété
+     */
     nbCours = 4;
+    /**
+     * Liste de cours choisis par l'utilisateur
+     */
     coursChoisi = [];
 
+    /**
+     * Constructeur
+     * @param {DOMElement} elementParent  Point d'insertion du DOM
+     
+     */
     constructor(elementParent){
         this.elementParent = elementParent;
         //this.validation();
     }
-
+    /**
+     * permet de lancer la validation du composant. Appelé par App
+     * @returns boolean
+     */
     validation(){
         let valide = false;
+
         console.log("validation")
-        let options = document.querySelectorAll("select option:checked");
-        console.log(this)
-        this.coursChoisi = [];
+        
+        let options = document.querySelectorAll("select option:checked"); // Récupère tous les éléments choisis
+        //console.log(this)
+        this.coursChoisi = [];  // Vide la liste
+        
         options.forEach((choix)=>{
-            console.log(this)
-            this.coursChoisi.push(choix.value); 
+        //    console.log(this)
+            this.coursChoisi.push(choix.value);     // Ajoute la valeur pour chaque cours choisi
         });
+        // Vérifie que le nombre est valide
         if(this.nbCours == options.length){
             valide = true;
         }
-        console.log(this.coursChoisi)
+        //console.log(this.coursChoisi)
         return valide;
     }
 
+    /**
+     * Valeur à retourner
+     * @todo - Finaliser cette méthode
+     * @returns 
+     */
     getData(){
 
         return {cours : ["582-31F", "582-31B"]};
     }
 
+    /**
+     * Méthode appelée par App pour passer des informations au composant
+     * @param {object} donnees 
+     */
     setData(donnees){
         this.nbCours = 4;
         if(donnees.nbCours){
@@ -37,6 +65,7 @@ export default class Inscription{
         }
         
     }
+    
     afficher(){
         let choixCours = cours.map(function(unCours){
             return `<option value="${unCours.sigle}">${unCours.nom}</option>`;
