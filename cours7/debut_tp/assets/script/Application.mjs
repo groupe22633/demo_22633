18b8664740af60b1f6ad1ee18ec672bd5c992d5b
Jonathan Martel <jonathmartel@gmail.com>
2023-02-27 14:57:04 -0500

    Cours 7 : debut tp

--- cours7/debut_tp/assets/script/Application.mjs
+++ cours7/debut_tp/assets/script/Application.mjs
@@ -5,7 +5,39 @@ import Filtre from "./Filtre.mjs";
 
 
 export default class Application{
-    
+    #catalogue;
+    constructor(){
+        const domCatalogue = document.querySelector(".catalogue"); 
+        this.#catalogue = new Catalogue(domCatalogue);
+        
+        this.#catalogue.setFilms(dataGhibli);
 
+        this.#catalogue.afficher();
+
+        let sectionFiltre = document.querySelector(".liste-categorie");
+        sectionFiltre.addEventListener("click", this.appliquerFiltre.bind(this));
+    }
+
+    appliquerFiltre(evt){
+        if(evt.target.classList.contains("choixFiltre")){
+            console.log(evt.target.dataset);
+            
+            const cat = evt.target.dataset.jsCat;
+            const valeur = evt.target.dataset.jsCatValeur;
+            
+            // appliquer un filtre ?
+            let mesFilms = dataGhibli.filter((unFilm)=>{
+                let valide = false;
+                if(unFilm[cat] == valeur){
+                    valide = true;
+                }
+                return valide;
+            })
+            
+            console.log(mesFilms)
+            this.#catalogue.setFilms(mesFilms);
+            this.#catalogue.afficher();
+        }
+    }
 
 }
--- cours7/debut_tp/assets/script/Catalogue.mjs
+++ cours7/debut_tp/assets/script/Catalogue.mjs
@@ -1,4 +1,34 @@
-export default class Catalogue{
-   
+export default class Catalogue {
     
+    #aFilms = [];
+
+    constructor(domParent) {
+        this.domParent = domParent;
+    }
+
+    setFilms(aFilms) {
+        this.#aFilms = aFilms;
+    }
+
+    afficher() {
+        console.log(this.#aFilms);
+        let chaineHTML = "";
+        this.#aFilms.forEach((unFilm)=>{
+            chaineHTML += ` <article class="carte">
+                    <header>
+                        <h2>${unFilm.title} (${unFilm.release_date})</h2>
+                        <h3>${unFilm.original_title}</h3>
+                    </header>
+                    <img src="${unFilm.image}">
+                    <div class="contenu">
+                        <p>${unFilm.description}</p>
+                    </div>
+                    <footer class="action">x, y z</footer>
+                </article>`;
+        })
+
+        this.domParent.innerHTML = chaineHTML;
+        
+    }
+
 }
