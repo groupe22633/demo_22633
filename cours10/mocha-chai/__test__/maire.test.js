2f74af21842ae8a8dff565d0807961f982b77ae9
Jonathan Martel <jonathmartel@gmail.com>
2023-03-10 09:58:41 -0500

    A la pause

--- cours10/mocha-chai/__test__/maire.test.js
+++ cours10/mocha-chai/__test__/maire.test.js
@@ -1,85 +1,77 @@
+import {mairesMTL} from "../src/mairesdata.js";
 import Maire from "../src/Maire.mjs";
-import { mairesMTL } from "../src/mairesdata.js";
 
 mocha.setup("bdd");
 const assert = chai.assert;
 const expect = chai.expect;
 
-describe("Test des méthodes de la classe Maire", () => {
-    const oMaire = new Maire(mairesMTL);
-    describe("Maire.getNombreMaires()", () => {
-        it("Vérifier que la fonction retourne un nombre", () => {
-            expect(oMaire.getNombreMaires()).to.be.an("Number");
-        });
-        it("Vérifier que le nombre de maire est valide", () => {
-            const expectLongueur = mairesMTL.length;
-            expect(oMaire.getNombreMaires()).to.be.equal(expectLongueur);
-            //assert.equal(oMaire.getNombreMaires(), expectLongueur);
-        });
-
+describe("Test des données de base", ()=> {
+    it("Est-ce que j'ai 55 enregistrements dans le tableau?", ()=> {
+        //assert.equal(mairesMTL.length, 55);
+        //expect(mairesMTL.length).to.be.equal(55);
+        expect(mairesMTL).to.be.an('array').and.also.to.have.lengthOf(55);
     });
 
-    describe("Maire.rechercheMaires()", () => {
-        it("Par date : 1800 (0 maire)", () => {
-            let param = { type: "date", valeur: 1800 };
-            expect(oMaire.rechercheMaires(param))
-                .to.be.an("Array")
-                .to.have.lengthOf(0);
-        });
-        it("Par date : 2030 (1 maire)", () => {
-            let param = { type: "date", valeur: 2030 };
-            expect(oMaire.rechercheMaires(param))
-                .to.be.an("Array")
-                .to.have.lengthOf(1);
-        });
-        it("Par date : 2013 (3 maires)", () => {
-            let param = { type: "date", valeur: 2013 };
-            expect(oMaire.rechercheMaires(param))
-                .to.be.an("Array")
-                .to.have.lengthOf(3);
-        });
+    it("Contient Valérie Plante", ()=> {
+        const val = {nom:"plante", prenom : "Valérie", debut : 2017, fin:null};
+        expect(mairesMTL).to.deep.include(val);
+    });
+})
 
-
+describe("Test de la classe Maire", ()=>{
+    describe("méthode getNombreMaire()", ()=>{
+        it("Vérifier que la fonction retourne un nombre (Maire())", ()=>{
+            let oMaire = new Maire();
+            expect(oMaire.getNombreMaires()).to.be.an('number');
+        })
+        it("Vérifier que la fonction retourne un nombre (Maire([]))", ()=>{
+            let oMaire = new Maire([]);
+            expect(oMaire.getNombreMaires()).to.be.an('number');
+        })
+        it("Vérifier que la fonction retourne un nombre (Maire(maireMTL))", ()=>{
+            let oMaire = new Maire(mairesMTL);
+            expect(oMaire.getNombreMaires()).to.be.an('number');
+        })
+        it("Vérifier que la fonction retourne la longueur du tableau", ()=>{
+            let oMaire = new Maire(mairesMTL);
+            const longueurAttendu = mairesMTL.length;
+            expect(oMaire.getNombreMaires()).to.equal(longueurAttendu);
+        })
     })
 
-    describe("Maire.listeMaires()", () => {
-        it("Par date : ASC", () => {
-            let param = { type: "date", ordre: "ASC" };
+    describe("Maire.listeMaire()", ()=>{
+        it("Par date, ASC", ()=>{
+            const param = {type:"date", ordre: "ASC"};
+            let oMaire = new Maire(mairesMTL);
             expect(oMaire.listeMaires(param))
-                .to.be.an("Array")
+                .to.be.an("array")
                 .to.have.lengthOf(mairesMTL.length);
-
+            
             expect(oMaire.listeMaires(param))
                 .to.have.members(mairesMTL);
-            
-            let data = [
-            {
-                "nom": "Viger",
-                "prenom": "Jacques",
-                "debut": 1833,
-                "fin": 1836
-            },
-            {
-                "nom": "McGill",
-                "prenom": "Peter",
-                "debut": 1840,
-                "fin": 1842
-            }];
-            
+            const membres = [
+                {
+                    "nom": "Viger",
+                    "prenom": "Jacques",
+                    "debut": 1833,
+                    "fin": 1836
+                },
+                {
+                    "nom": "McGill",
+                    "prenom": "Peter",
+                    "debut": 1840,
+                    "fin": 1842
+                }
+            ]
             expect(oMaire.listeMaires(param))
-                .to.deep.include.ordered.members(data);
-        });
-
-        it('Par : { type: "data", ordre: "" }', () => {
-            let param = { type: "data", ordre: "" };
-            expect(oMaire.listeMaires(param))
-                .to.be.an("Array")
-                //.to.have.lengthOf(0);
-        });
+                .to.deep.include.ordered.members(membres);
+        })
     })
-});
+})
 
 mocha.run();
+
+
 
 
 
