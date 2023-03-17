7e6c52c42476c0cd2c2100f4547701d9071071bc
Jonathan Martel <jonathmartel@gmail.com>
2023-03-17 11:03:28 -0400

    Fin du cours 11

--- cours11/fetch/assets/script/Ghibli.mjs
+++ cours11/fetch/assets/script/Ghibli.mjs
@@ -8,7 +8,10 @@ export default class Ghibli{
      * @param {function} cb - Fonction de rappel qui prendra les données en paramètre     
      */
     getFilms(cb){
-       
+       // À essayer avec fetch
+       /**
+        * fetch. then .then ( cb(data))
+        */
 
     }
     /**
@@ -28,11 +31,17 @@ export default class Ghibli{
 
     /**
      * 
-     * @param {function} cb 
+     * @param {function} cb - Fonction de rappel
      */
     getFilmsXHR(cb){
-       
-        
+       const xhr = new XMLHttpRequest();
+       xhr.addEventListener("load", function(){
+        const data = JSON.parse(xhr.response);
+        cb(data);
+        //console.log(xhr.response)
+       })
+       xhr.open("GET", this.URLGhibli+ "films");
+       xhr.send();
     }
 
     
